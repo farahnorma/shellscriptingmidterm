65ada07a61db49baec83f38b286e483d74e46987
EC2 Default User <ec2-user@ip-172-31-81-173.ec2.internal>
2019-10-17 00:49:56 +0000

    midterm -metadata

--- midterm.sh
+++ midterm.sh
@@ -4,11 +4,11 @@
 
 function find_metadata() {
 touch metadata.txt
-curl -w "\n" http://169.254.169.254/latest/meta-data/ami-id
-curl -w "\n" http://169.254.169.254/latest/meta-data/instance-id
-curl -w "\n" http://169.254.169.254/latest/meta-data/hostname
-curl -w "\n" http://169.254.169.254/latest/meta-data/security-groups
-curl -w "\n" http://169.254.169.254/latest/meta-data/public-hostname
+curl -w "\n" http://169.254.169.254/latest/meta-data/ami-id>>metadata.txt
+curl -w "\n" http://169.254.169.254/latest/meta-data/instance-id>>metadata.txt
+curl -w "\n" http://169.254.169.254/latest/meta-data/hostname>>metadata.txt
+curl -w "\n" http://169.254.169.254/latest/meta-data/security-groups>>metadata.txt
+curl -w "\n" http://169.254.169.254/latest/meta-data/public-hostname>>metadata.txt
 }
 
 
